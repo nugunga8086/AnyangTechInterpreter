31ad07b7299f8ec87d807ecdd4570497d06e1c62
nugunga8086 <https://nugunga8086@github.com>
2020-12-14 10:57:31 +0900

    main

---
+++ QuantumCompilerInterPreter_Main/CMake/QCI.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../QCI"
+  "../QCI.pdb"
+  "CMakeFiles/../QCI.dir/home/nugunga/Documents/git_repos/QuantumCompilerInterpreter/QuantumCompilerInterPreter_Main/Source/QCI_Start.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/../QCI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
