2ff2cd1dcef64e75a541e84db669b1e1a9ddf784
nugunga8086 <nugunga8086@github.com>
2021-01-15 16:04:45 +0900

    main

--- interpreter/Cmake/cmake_install.cmake
+++ interpreter/Cmake/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/nugunga/Desktop/interpreter/Cmake
+# Install script for directory: /home/nugunga/Desktop/git/QuantumCompilerInterpreterTest/interpreter/Cmake
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/nugunga/Desktop/interpreter/Cmake/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/nugunga/Desktop/git/QuantumCompilerInterpreterTest/interpreter/Cmake/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
